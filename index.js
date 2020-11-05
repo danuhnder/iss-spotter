d9b9194f31e0d6bdf96c40d42dd47a5d2f9b71ad
Daniel Robertson <danieljrobertson@gmail.com>
2020-11-05 23:53:15 +0000

    full functionality implemented huzzah

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // index.js
-const { fetchMyIP, fetchCoordsByIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes} = require('./iss');
 
 // fetchCoordsByIP('99.238.239.2', (error, data) => {
 //   if (error) {
@@ -19,3 +19,22 @@ const { fetchMyIP, fetchCoordsByIP } = require('./iss');
 
 //   console.log('It worked! Returned IP:' , ip);
 // });
+
+// fetchISSFlyOverTimes({latitude: 43.6456, longitude: -79.4569}, (error, times) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned flyover times:' , times);
+// });
+
+const { nextISSTimesForMyLocation } = require('./iss');
+
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  // success, print out the deets!
+  console.log(passTimes);
+});
--- iss.js
+++ iss.js
@@ -50,4 +50,81 @@ const fetchCoordsByIP = function(ip, callback) {
   });
 };
 
-module.exports = { fetchMyIP, fetchCoordsByIP };
+const fetchISSFlyOverTimes = function(coords, callback) {
+  request(`http://api.open-notify.org/iss-pass.json?lat=${coords.latitude}&lon=${coords.longitude}&n=6`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode } when fetching ISS flyover times. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+    const data = JSON.parse(body);
+    if (data.message === "fail") {
+      console.log("ISS EXPLOSION lol just kidding but check your inputs");
+      return;
+    }
+    const locationData = data.response;
+    callback(null, locationData);
+    
+  });
+};
+
+/**
+ * Orchestrates multiple API requests in order to determine the next 5 upcoming ISS fly overs for the user's current location.
+ * Input:
+ *   - A callback with an error or results. 
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The fly-over times as an array (null if error):
+ *     [ { risetime: <number>, duration: <number> }, ... ]
+ */ 
+const nextISSTimesForMyLocation = function(callback) {
+  fetchMyIP((error, ip) => {
+    if (error) {
+      console.log("failure @fetchIP" , error);
+      return;
+    }  
+    fetchCoordsByIP(ip, (error, data) => {
+      if (error) {
+        console.log("failure @fetchcoords" , error);
+        return;
+      }
+      fetchISSFlyOverTimes(data, (error, times) => {
+        if (error) {
+          console.log("failure @fetchflyover!" , error);
+          return;
+        }
+        for (let event of times) {
+         dateConverter(event);
+        }
+      });
+    });
+  });
+}
+const dateConverter = (obj) => {
+  const dur = obj.duration;
+  const UTXdate = new Date(obj.risetime*1000); 
+  const months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+  const year = UTXdate.getFullYear();
+  const month = months[UTXdate.getMonth()];
+  const day = UTXdate.getDay();
+  const date = UTXdate.getDate();
+  const hour = UTXdate.getHours();
+  const min = UTXdate.getMinutes();
+  const sec = UTXdate.getSeconds();
+  const time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
+  console.log(`Next flyover is ${time} for ${dur} seconds`);
+  
+  
+  
+  
+  
+  
+  // const readableDate = UTXdate.toLocaleString("en-US", {weekday: "long", timeZoneName: "short"});
+  // console.log(readableDate + dur);
+}
+
+module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes, nextISSTimesForMyLocation };
